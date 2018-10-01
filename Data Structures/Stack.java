514e688ff1cfd0305f5400326c9c38286c84b644
Harsh <aroraharsh010@gmail.com>
2018-10-01 21:13:22 +0530

    Added stack.java

---
+++ Data Structures/Stack.java
@@ -0,0 +1,69 @@
+package pep20_17Mar;
+
+public class Stack {
+
+	private int[] data;
+	private int tos = -1;// top of stackk 
+
+	public Stack(int capacity) {
+		this.data = new int[capacity];
+	}
+
+	public Stack()// contructor chaining
+	{
+		this(5);
+	}
+
+	public int top()// tells element at tos
+	{
+		if (this.tos == -1) {
+			System.out.println("Stack UnderFlow");
+			return -1;
+		}
+		int value = this.data[this.tos]; 
+		return value;
+	}
+
+	public int pop()// element to delete
+	{
+		// check for underflow
+		if (this.tos == -1) {
+			System.out.println("Stack UnderFlow");
+			return -1;
+		}
+		int value = this.data[this.tos];
+		this.data[this.tos] = 0;
+		this.tos--;
+		return value;
+
+	}
+
+	public void push(int item) {
+		// check for overflow
+		if (this.tos == this.data.length - 1) {
+			System.out.println("Stack OverFlow");
+			return;
+		}
+		this.tos++;
+		this.data[this.tos] = item;
+	}
+
+	public boolean isEmpty() {
+		if (this.size() == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public void display() {
+		for (int i = tos; i >= 0; i--) {
+			System.out.print(this.data[i] + " "); 
+		}
+		System.out.println();
+	}
+
+	public int size() {
+		return this.tos + 1;
+	}
+
+}
