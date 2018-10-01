60cd714c8924fafd10b69e77232d205aa14283f6
Aks1307 <40080782+Aks1307@users.noreply.github.com>
2018-10-02 00:02:36 +0530

    jumpSearch.cpp

---
+++ searching/jumpSearch.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+int jumpSearch(int arr[], int x, int n) 
+{ 
+    // Finding block size to be jumped 
+    int step = sqrt(n); 
+  
+    // Finding the block where element is 
+    // present (if it is present) 
+    int prev = 0; 
+    while (arr[min(step, n)-1] < x) 
+    { 
+        prev = step; 
+        step += sqrt(n); 
+        if (prev >= n) 
+            return -1; 
+    } 
+  
+    // Doing a linear search for x in block 
+    // beginning with prev. 
+    while (arr[prev] < x) 
+    { 
+        prev++; 
+  
+        // If we reached next block or end of 
+        // array, element is not present. 
+        if (prev == min(step, n)) 
+            return -1; 
+    } 
+    // If element is found 
+    if (arr[prev] == x) 
+        return prev; 
+  
+    return -1; 
+} 
